3ddacb6aeba5f7010c24f6e17b77289903c79b46
rratneshSingh <work.ratnesh@gmail.com>
2025-11-20 21:08:57 +0530

    project

--- project/arrival.js
+++ project/arrival.js
@@ -3,59 +3,159 @@ const busstopcode = params.get('BusStopCode');
 const requestOptions = {
     method: "GET",
 };
-const busStop = document.getElementById('busstop');
-const h3 = document.createElement('h3');
-h3.innerText = 'Bus Stop Code: ' + busstopcode;
-busStop.appendChild(h3);
-const div = document.createElement('div');
+
 fetch(`http://localhost:3000/ltaodataservice/v3/BusArrival?BusStopCode=${busstopcode}`)
     .then((response) => response.json())
     .then((result) => {
-        debugger;
-        const services = result.Services;
-        for (let j = 0; j < services.length; j = j + 1) {
-            const service = services[j];
-            const h5 = document.createElement('h5');
-            h5.innerText = 'Service No: ' + service.ServiceNo;
-            div.appendChild(h5);
-            const keys = Object.keys(service);
-            const table = document.createElement('table');
-            const htr = document.createElement('tr');
-            const thBusCode = document.createElement('td');
-            thBusCode.innerText = 'Bus name';
-            htr.appendChild(thBusCode);
-            const thArrival = document.createElement('td');
-            thArrival.innerText = 'Arrival';
-            htr.appendChild(thArrival);
-            const thOriginCode = document.createElement('td');
-            thOriginCode.innerHTML = 'Origin Code';
-            htr.appendChild(thOriginCode);
-            const thDestinationCode = document.createElement('td');
-            thDestinationCode.innerText = 'Destination code';
-            htr.appendChild(thDestinationCode);
-            table.appendChild(htr);
-            for (let i = 0; i < keys.length; i = i + 1) {
-                const busData = service[keys[i]];
-                if (busData && busData.OriginCode) {
-
-                    const tr = document.createElement('tr');
-                    const tdBusCode = document.createElement('td');
-                    tdBusCode.innerText = keys[i];
-                    const tdArrival = document.createElement('td');
-                    tdArrival.innerText = busData.EstimatedArrival;
-                    const tdOriginCode = document.createElement('td');
-                    tdOriginCode.innerText = busData.OriginCode;
-                    const tdDestinationCode = document.createElement('td');
-                    tdDestinationCode.innerText = busData.DestinationCode;
-                    tr.appendChild(tdBusCode);
-                    tr.appendChild(tdArrival);
-                    tr.appendChild(tdOriginCode);
-                    tr.appendChild(tdDestinationCode);
-                    table.appendChild(tr);
-                }
-            }
-            div.appendChild(table);
+        if (!busstopcode) {
+            return;
         }
-        busStop.appendChild(div);
+        const busStopData = document.getElementById('busstop');
+        const busStopCard = document.createElement('div');
+        busStopCard.classList.add('busstop_card');
+        const busStopInfo = document.createElement('div');
+        busStopInfo.classList.add('busstop_info');
+        const address = document.createElement('div');
+        address.classList.add('address');
+        address.innerText = 'Bus Stop: ' + result.BusStopCode;
+        const favIcon = document.createElement('span');
+        favIcon.classList.add('material-symbols-outlined');
+        favIcon.innerText = 'favorite';
+        busStopInfo.appendChild(address);
+        busStopInfo.appendChild(favIcon);
+        busStopCard.appendChild(busStopInfo);
+        busStopData.appendChild(busStopCard);
+
+        const busStopDetails = document.createElement('div');
+        address.classList.add('busstop_bus_details');
+
+        const table = document.createElement('table');
+        const headingRow = document.createElement('tr');
+        const busNoColumn = document.createElement('td');
+        busNoColumn.innerText = 'Bus no.'
+        const nextBusColumn = document.createElement('td');
+        nextBusColumn.innerText = 'Next';
+        const subSeqColumn = document.createElement('td');
+        subSeqColumn.innerText = 'Subseq.'
+        headingRow.appendChild(busNoColumn);
+        headingRow.appendChild(nextBusColumn);
+        headingRow.appendChild(subSeqColumn);
+        table.appendChild(headingRow);
+        busStopDetails.appendChild(table);
+        busStopCard.appendChild(busStopDetails);
+
+        const services = result.Services;
+        for (let i = 0; i < services.length; i = i + 1) {
+            const cuurentDateInMilisec = (new Date()).getTime();
+            const service = services[i];
+            const tr = document.createElement('tr');
+            const busNotd = document.createElement('td');
+            busNotd.innerText = service.ServiceNo;
+            const nexttd = document.createElement('td');
+            if (service.NextBus.EstimatedArrival) {
+                const nextbusDateInMilliSec = new Date(service.NextBus.EstimatedArrival);
+                const diff = nextbusDateInMilliSec - cuurentDateInMilisec;
+                if (diff < 1) {
+                    nexttd.innerText = 'Arriving';
+                } else {
+                    const minutes = diff / 60000;
+                    nexttd.innerText = Math.floor(minutes) + 'min';
+                }
+            } else {
+                nexttd.innerText = '-';
+            }
+            const subseqtd = document.createElement('td');
+            if (service.NextBus2.EstimatedArrival) {
+                const subseqbusDateInMilliSec = new Date(service.NextBus2.EstimatedArrival);
+                const diff1 = subseqbusDateInMilliSec - cuurentDateInMilisec;
+                if (diff1 < 1) {
+                    subseqtd.innerText = 'Arriving';
+                } else {
+                    const minutes1 = diff1 / 60000;
+                    subseqtd.innerText = Math.floor(minutes1) + 'min';
+                }
+            } else {
+                subseqtd.innerText = '-';
+            }
+            tr.appendChild(busNotd);
+            tr.appendChild(nexttd);
+            tr.appendChild(subseqtd);
+            table.appendChild(tr);
+        }
+
     })
     .catch((error) => console.error(error));
+
+
+result = {
+    "BusStopCode": "55051",
+    "Services": [
+        {
+            "ServiceNo": "163",
+            "NextBus": {
+                "EstimatedArrival": "2025-11-20T22:11:08+08:00",
+            },
+            "NextBus2": {
+                "EstimatedArrival": "2025-11-20T22:22:48+08:00",
+            },
+        },
+        {
+            "ServiceNo": "70M",
+            "NextBus": {
+                "EstimatedArrival": "2025-11-20T22:09:57+08:00",
+            },
+            "NextBus2": {
+                "EstimatedArrival": "",
+            }
+        }
+    ]
+}
+
+// <div class="busstop_card">
+//     <div class="busstop_info">
+//         <div class="address">Electronics</div>
+//
+//             <span class="material-symbols-outlined">
+//                 favorite
+//             </span>
+//     </div>
+//     <div class="busstop_bus_details">
+//         <table>
+//             <tr>
+//                 <th>Company</th>
+//                 <th>Contact</th>
+//                 <th>Country</th>
+//             </tr>
+//             <tr>
+//                 <td>Alfreds Futterkiste</td>
+//                 <td>Maria Anders</td>
+//                 <td>Germany</td>
+//             </tr>
+//             <tr>
+//                 <td>Centro comercial Moctezuma</td>
+//                 <td>Francisco Chang</td>
+//                 <td>Mexico</td>
+//             </tr>
+//             <tr>
+//                 <td>Ernst Handel</td>
+//                 <td>Roland Mendel</td>
+//                 <td>Austria</td>
+//             </tr>
+//             <tr>
+//                 <td>Island Trading</td>
+//                 <td>Helen Bennett</td>
+//                 <td>UK</td>
+//             </tr>
+//             <tr>
+//                 <td>Laughing Bacchus Winecellars</td>
+//                 <td>Yoshi Tannamuri</td>
+//                 <td>Canada</td>
+//             </tr>
+//             <tr>
+//                 <td>Magazzini Alimentari Riuniti</td>
+//                 <td>Giovanni Rovelli</td>
+//                 <td>Italy</td>
+//             </tr>
+//         </table>
+//     </div>
+// </div>
